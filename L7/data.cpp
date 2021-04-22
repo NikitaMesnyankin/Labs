71e7ed3bc24eb513ae567a50b095ef8308e2e6f3
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2021-04-22 12:45:16 +0300

    Fix typos in messages

--- L7/data.cpp
+++ L7/data.cpp
@@ -10,7 +10,7 @@ int mesnyankin::createBinaryFile(const int arg, char *arguments[])
         return 1;
     }
 
-    FILE *ft = fopen(arguments[2], "rt+");
+    FILE *ft = fopen(arguments[2], "rt");
     if (ft == NULL) {
         std::cout << "Error: can't open file with data!\n" << arguments[2];
         std::cout << "Press any key";
@@ -30,15 +30,14 @@ int mesnyankin::createBinaryFile(const int arg, char *arguments[])
     Mountain mntn;
     int kol = 0, nw = 1;
     while (nw) {
-        fscanf_s(ft,"%30s", mntn.Name);
+        fscanf(ft,"%30s", mntn.Name);
         if (strcmp(mntn.Name, "**") == 0 || feof(ft)) {
             break;
         }
 
-        fscanf_s(ft,"%d", mntn.longtitude);
-        fscanf_s(ft,"%d", mntn.latitude);
-        fscanf_s(ft,"%d", mntn.altitude);
-
+        fscanf(ft,"%d", &mntn.longtitude);
+        fscanf(ft,"%d", &mntn.latitude);
+        fscanf(ft,"%d", &mntn.altitude);
         nw = fwrite(&mntn, sizeof(Mountain), 1, fb);
         kol++;
     }
@@ -50,7 +49,6 @@ int mesnyankin::createBinaryFile(const int arg, char *arguments[])
     fclose(fb);
 
     std::cout << "Created binary file with " << kol << "record per " << sizeof(Mountain) <<  "bytes\n";
-    std::cout << "Press any key to continue";
     system("pause");
     return 0;
 }
@@ -58,7 +56,7 @@ int mesnyankin::createBinaryFile(const int arg, char *arguments[])
 int mesnyankin::findRecord(const int arg, char *arguments[])
 {
     if (arg < 2) {
-        std::cout << "Мало параметров\nPress any key";
+        std::cout << "Not enough parameters\nPress any key";
         system("pause");
         return 1;
     }
@@ -72,14 +70,12 @@ int mesnyankin::findRecord(const int arg, char *arguments[])
     }
 
     Mountain mntn;
-    char SUp[31];
     int kol = 0, nr = 1;
     while (nr) {
         nr = fread(&mntn, sizeof(Mountain), 1, fb);
         if (nr > 0)
-            if (mntn.latitude > 0 && mntn.altitude > 3000) {
-                std::cout << "Found record: " << mntn.Name, " | A:" , \
-                        mntn.altitude, " | LA / LO: ", mntn.latitude, " / ", mntn.longtitude;
+            if ((mntn.latitude > 0) && (mntn.longtitude > 3000)) {
+                std::cout << "Found record: " << mntn.Name << " | LO:"  << mntn.longtitude << " | LA / AL: " << mntn.latitude << " / " << mntn.altitude << "\n";
                 kol++;
             }
     }
@@ -90,8 +86,6 @@ int mesnyankin::findRecord(const int arg, char *arguments[])
         std::cout << "Found: " << kol;
 
     fclose(fb);
-
-    std::cout << "Press any key to continue";
     system("pause");
     return 0;
 }
@@ -106,7 +100,7 @@ int mesnyankin::correctFile(const int arg, char *arguments[])
 
     FILE *fb = fopen(arguments[1], "rb+");
     if (fb == NULL) {
-        std::cout << "Error: не удалось открыть двоичный файл: ", arguments[1];
+        std::cout << "Error: couldn't open binary file: ", arguments[1];
         system("pause");
         return 1;
     }
@@ -115,8 +109,7 @@ int mesnyankin::correctFile(const int arg, char *arguments[])
     int kol=0, nr=1, nw=1, flag;
     nr = fread(&mntn, sizeof(Mountain), 1, fb);
     while (nr == 1 && nw == 1) {
-        std::cout << "Found data: " << mntn.Name, " | A:" , \
-                        mntn.altitude, " | LA / LO: ", mntn.latitude, " / ", mntn.longtitude;
+        std::cout << "Found data: " << mntn.Name << " | LO:"  << mntn.longtitude << " | LA / AL: " << mntn.latitude << " / " << mntn.altitude << "\n";
 
         flag=0;
         if (mntn.latitude < -90) {
@@ -129,17 +122,17 @@ int mesnyankin::correctFile(const int arg, char *arguments[])
             flag=1;
         }
 
-        if (mntn.longtitude < -180) {
-            mntn.longtitude = -180;
+        if (mntn.altitude < -180) {
+            mntn.altitude = -180;
             flag=1;
         }
 
-        if (mntn.longtitude > 180) {
-            mntn.longtitude = 180;
+        if (mntn.altitude > 180) {
+            mntn.altitude = 180;
             flag=1;
         }
 
-        if (mntn.altitude < 0) {
+        if (mntn.longtitude < 0) {
             mntn.altitude = abs(mntn.altitude);
             flag=1;
         }
@@ -150,8 +143,7 @@ int mesnyankin::correctFile(const int arg, char *arguments[])
             nw = fwrite(&mntn, sizeof(mntn), 1, fb);
             fseek(fb, 0, SEEK_CUR);
 
-            std::cout << "Corrected: " << mntn.Name, " | A:" , \
-                        mntn.altitude, " | LA / LO: ", mntn.latitude, " / ", mntn.longtitude;
+            std::cout << "Corrected: " << mntn.Name << " | LO:"  << mntn.longtitude << " | LA / AL: " << mntn.latitude << " / " << mntn.altitude << "\n";
         }
         nr = fread(&mntn, sizeof(mntn), 1, fb);
     }
@@ -159,7 +151,7 @@ int mesnyankin::correctFile(const int arg, char *arguments[])
         std::cout << "No corrections\n";
     }
     else
-        std::cout << "Corrected: " << kol;
+        std::cout << "Corrected: " << kol << "\n";
 
     fclose(fb);
 
