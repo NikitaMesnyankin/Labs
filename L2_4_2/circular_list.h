11417a9c7823677777e2e81bb303d7cceb7b4032
Nikita_Mesnyankin <NikitaMesnyankin@gmail.com>
2021-10-24 15:49:05 +0300

    Rewrited gitignore

--- L2_4_2/circular_list.h
+++ L2_4_2/circular_list.h
@@ -1,12 +1,12 @@
-#ifndef CIRCULAR_H
-#define CIRCULAR_H
+//#ifndef CIRCULAR_H
+//#define CIRCULAR_H
 
 #include <list>
 #include <iterator>
 #include <string>
 #include <tuple>
 #include <iostream>
-#include <boost/circular_buffer.hpp>
+#include <boost\circular_buffer.hpp>
 
 typedef std::tuple<std::string, std::string, std::string, bool> target_tuple;
 
@@ -26,6 +26,7 @@ namespace mesnyankin
 		CircularList<T>& operator=(const CircularList<T>&);
 		CircularList<T>& operator=(CircularList<T>&&) noexcept;
 
+		boost::circular_buffer<T> getContainer() const;
 		void showData() const;
 		~CircularList<T>() = default;
 	private:
@@ -33,4 +34,70 @@ namespace mesnyankin
 	};
 }
 
-#endif
+template <typename T>
+mesnyankin::CircularList<T>::CircularList()
+{
+	this->buffer_ = boost::circular_buffer<T>();
+}
+
+template <typename T>
+mesnyankin::CircularList<T>::CircularList(const boost::circular_buffer<T>& buffer_data)
+{
+	this->buffer_ = buffer_data;
+}
+
+template<typename T>
+mesnyankin::CircularList<T>::CircularList(const CircularList<T>& rhs)
+{
+	this.buffer_ = rhs.buffer_;
+}
+
+
+template <typename T>
+mesnyankin::CircularList<T>::CircularList(CircularList<T>&& rhs) noexcept
+{
+	this.buffer_ = std::move(rhs.buffer_);
+}
+
+template <typename T>
+mesnyankin::CircularList<T>& mesnyankin::CircularList<T>::operator=(const CircularList<T>& rhs)
+{
+	if (this->buffer_ != rhs->buffer_)
+	{
+		this->buffer_(rhs->buffer_);
+	}
+	return *this;
+}
+
+template <typename T>
+mesnyankin::CircularList<T>& mesnyankin::CircularList<T>::operator=(CircularList<T>&& rhs) noexcept
+{
+	if (!(rhs.buffer_.empty()))
+	{
+		this->buffer_(rhs->buffer_);
+	}
+	return *this;
+}
+
+template<typename T>
+inline boost::circular_buffer<T> mesnyankin::CircularList<T>::getContainer() const
+{
+	return this->buffer_;
+}
+
+template <typename T>
+void mesnyankin::CircularList<T>::showData() const
+{
+	if (!(this->buffer_.empty()))
+	{
+		for (const T& item : buffer_)
+		{
+			std::apply([](auto&&... args)
+				{
+					((std::cout << args << '\n'), ...);
+				}, item);
+		}
+	}
+}
+
+//#endif
